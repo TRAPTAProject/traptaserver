8a293bc302eb36e60b87bd32ce914fd0a95950cf
Frederi Mahieu <frederi.mahieu@gmail.com>
2020-06-29 17:51:34 +0200

    Version 1.23

--- define.h
+++ define.h
@@ -1,8 +1,8 @@
 #ifndef DEFINE_H
 #define DEFINE_H
 
-#define VERSION "1.22"
-#define DATE "Janvier 2020"
+#define VERSION "1.23"
+#define DATE "Juin 2020"
 #define DBVERSION 6
 
 #endif // DEFINE_H
